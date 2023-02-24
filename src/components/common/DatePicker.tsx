56a7baa434f244ff13449374fc04823a4fba6bb4
hojin <cacabi24@hanmail.net>
2023-02-24 13:44:25 +0900

    🛠️ DatePicker locale 적용
    🛠️ DatePicker locale 적용

--- src/components/common/DatePicker.tsx
+++ src/components/common/DatePicker.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import dayjs, { Dayjs } from 'dayjs';
+import 'dayjs/locale/ko';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { DatePicker,  } from '@mui/x-date-pickers/DatePicker';
 
 interface Props {
   views: Array<any>;
@@ -14,12 +15,12 @@ interface Props {
 
 const CustomDatePicker = ({ views, value, setDate }: Props) => {
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={'ko'}>
       <Stack spacing={3}>
         <DatePicker
-          inputFormat={views.length === 1? 'YYYY' : "YYYY-MM"}
+          inputFormat={views.length === 1? 'YYYY년' : "YYYY년 MM월"}
           views={views}
-          label={views.length === 1? 'Year' : "Calendar"}
+          label={views.length === 1? '조회기간(년도)' : "조회기간(년월)"}
           minDate={dayjs('2018-01-01')}
           maxDate={dayjs('2021-12-01')}
           value={value}
